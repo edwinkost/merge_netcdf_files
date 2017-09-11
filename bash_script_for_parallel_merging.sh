c1dadf08045d12111410f180fe7cd542a47e49c9
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-09-11 12:19:19 +0200

    Test.

---
+++ bash_script_for_parallel_merging.sh
@@ -0,0 +1,5 @@
+
+python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outDailyTotNC 1958-01-01 1958-12-31 discharge NETCDF4 True 1 Global &
+python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outDailyTotNC 1959-01-01 1959-12-31 discharge NETCDF4 True 1 Global &
+wait
+
