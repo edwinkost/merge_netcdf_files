8707f3e8a7e606bb79070601f5331029567f710a
Edwin Sutanudjaja <edwin@workstationedwin.geo.uu.nl>
2017-09-11 12:32:47 +0200

    Test.

--- bash_script_for_parallel_merging.sh
+++ bash_script_for_parallel_merging.sh
@@ -1,11 +1,15 @@
 
-# daily resolution
-python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outDailyTotNC 1958-01-01 1958-12-31 discharge NETCDF4 True 1 Global &
-python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outDailyTotNC 1959-01-01 1959-12-31 discharge NETCDF4 True 1 Global &
-wait
-
 # monthly resolution - cumulative value
 python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outMonthTotNC 1958-01-01 1958-12-31 gwRecharge,totalRunoff,totalGroundwaterAbstraction NETCDF4 True 3 Global &
 
 # monthly resolution - average value
 python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outMonthAvgNC 1958-01-01 1958-12-31 channelStorage,discharge NETCDF4 True 3 Global &
+
+wait
+
+# daily resolution
+python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outDailyTotNC 1958-01-01 1958-12-31 discharge NETCDF4 True 1 Global &
+python merge_netcdf.py /projects/0/aqueduct/users/edwinsut/pcrglobwb_runs_2016_oct_nov/pcrglobwb_4_land_covers_edwin_parameter_set_cru-ts3.23_era-20c_kinematicwave/no_correction/non-natural/begin_from_1958/ /scratch-shared/edwinhs/demo_for_joost/ outDailyTotNC 1959-01-01 1959-12-31 discharge NETCDF4 True 1 Global &
+
+wait
+
